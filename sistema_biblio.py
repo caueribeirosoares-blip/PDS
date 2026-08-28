10d809b719babd21475929afeee2115def3f9b36
caueribeirosoares-blip <caue.ribeiro.soares@escola.pr.gov.br>
2026-08-28 10:37:58 -0300

    Create sistema_biblio.py

---
+++ sistema_biblio.py
@@ -0,0 +1,25 @@
+class Livro:
+    def __init__(self, titulo, autor, paginas):
+        self.titulo = titulo
+        self.autor = autor
+        self.paginas = paginas
+
+    def __str__(self):
+        return f"Título: {self.titulo}\nAutor: {self.autor}\nPáginas: {self.paginas}"
+        
+    def __eq__(self, outro):
+    return self.isbn == outro.isbn
+
+
+# Solicita os dados ao usuário
+titulo_informado = input("Digite o título do livro: ")
+autor_informado = input("Digite o autor do livro: ")
+paginas_informadas = input("Digite a quantidade de páginas: ")
+
+# Cria a instância do livro
+novo_livro = Livro(titulo_informado, autor_informado, paginas_informadas)
+
+# Exibe a descrição formatada utilizando o método __str__
+print("\n--- Conferência dos Dados ---")
+print(novo_livro)
+
